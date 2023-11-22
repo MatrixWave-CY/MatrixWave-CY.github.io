43cfd3e70ee6fcb5384218301ce1821d4cf23d49
MatrixWave-CY <pcyggyx@gmail.com>
2023-11-23 01:41:35 +0800

    Site updated: 2023-11-23 01:41:34

---
+++ status/config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm795502335-5fffac882cfcd571581124b8',
+    'm795636677-e54904ff4e8b9afa2a2e4eaa',
+    'm795636687-8db988c5ac1f2ed7e3d2b92a',
+    // 'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://stats.uptimerobot.com/mmPnkF1ANM'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://nex18.top/'
+    },
+  ],
+};
---
+++ status/index.md
@@ -0,0 +1,5 @@
+---
+title: status
+date: 2023-11-23 01:01:37
+type: status
+---
